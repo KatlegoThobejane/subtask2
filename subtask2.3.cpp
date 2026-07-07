a6b6ff2b027a2d6df4c15b23193d7d8aa4c5ebe0
unknown <202515305kt@gmail.com>
2026-07-07 14:50:57 +0200

    using loops

---
+++ subtask2.3.cpp
@@ -0,0 +1,14 @@
+ cout << "\nFull Morse code
+   Message:\n";
+     for (char c : MorseCode) {
+          if (c == '/')
+               cout << "/";
+             else
+                cout << c;
+    }
+    
+    cout << endl;
+    
+    return 0;
+    
+ }
